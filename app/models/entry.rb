db8b2523c8b03b0f0007cf81d6f11967d23449d3
Rod <rcardenas@utexas.edu>
2016-08-02 19:30:08 -0500

    Complete Euclidian Distance Guess.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -14,29 +14,20 @@
 class Entry < ActiveRecord::Base
   validates :height, :weight, presence: :true
 
-  MAX_HEIGHT = 10
-  MAX_WEIGHT = 500
-
   def self.eucladianDistanceGuess(height, weight)
-    puts "here"
-    # entries = Entry.all
-    # entries.each do |entry|
-    #   entry.height = height / MAX_HEIGHT
-    #   entry.weight = weight / MAX_WEIGHT
-    # end
-    height.to_i > 5 ? true : false
+    entries = Entry.all
+    max_similarity_distance = 0
+    closest_match = nil
+
+    entries.each do |entry|
+      similarity_distance = 1/(1 + Math.sqrt((entry.height - height.to_i) ** 2 + (entry.weight - weight.to_i) ** 2))
+      if similarity_distance > max_similarity_distance
+        max_similarity_distance = similarity_distance
+        closest_match = entry.catLover
+      end
+    end
+
+    closest_match
   end
 
-  # def sim_distance(prefs,person1,person2):
-  #     # Get the list of shared_items
-  #     si={}
-  #     for item in prefs[person1]:
-  #       if item in prefs[person2]:
-  #           si[item]=1
-  #     # if they have no ratings in common, return 0
-  #     if len(si)==0: return 0
-  #     # Add up the squares of all the differences
-  #     sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2)
-  #                         for item in prefs[person1] if item in prefs[person2]])
-  #     return 1/(1+sum_of_squares)
 end
